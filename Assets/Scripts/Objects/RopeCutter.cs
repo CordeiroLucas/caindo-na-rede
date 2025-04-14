a1dace67b165586f69565e685fea3a3940b28260
CordeiroLucas <pacheco.lucascordeiro@gmail.com>
2025-04-14 10:09:16 -0300

    feat: Ignore collision with ball

--- Assets/Scripts/Objects/RopeCutter.cs
+++ Assets/Scripts/Objects/RopeCutter.cs
@@ -4,7 +4,21 @@ public class RopeCutter : MonoBehaviour
 {
     Vector2 worldMousePosition;
 
-    void Update()
+    public GameObject objectToIgnore;
+
+    void Start()
+    {
+        if (objectToIgnore != null)
+        {
+            Physics2D.IgnoreCollision(GetComponent<Collider2D>(), objectToIgnore.GetComponent<Collider2D>());
+        } 
+        else
+        {
+            Debug.Log("Objeto ignorado não encontrado");
+        }
+    }
+
+    void FixedUpdate()
     {
         worldMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = worldMousePosition;
